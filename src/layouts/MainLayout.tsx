f040b210a05963444aae90cd43bf49c0c94661f3
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2025-03-13 14:53:40 +0530

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,13 +6,18 @@ import Player from '../components/Player';
 const MainLayout: React.FC = () => {
   return (
     <div className="flex h-screen bg-gray-900 text-white">
+      {/* Sidebar */}
       <Sidebar />
-      
-      <main className="flex-1 overflow-y-auto pb-24">
+
+      {/* Main Content */}
+      <main className="flex-1 overflow-y-auto pb-24 md:pb-0 md:pl-64 transition-all duration-300">
         <Outlet />
       </main>
-      
-      <Player />
+
+      {/* Player */}
+      <div className="fixed bottom-0 left-0 right-0 md:left-64 z-50">
+        <Player />
+      </div>
     </div>
   );
 };
